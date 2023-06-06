d29eda856b81f894c83b3c02b6cf30acf62699d9
floor12 <floor12@floor12.net>
2023-06-07 00:53:08 +0200

    Video part of banner updated to fast load.

--- src/widgets/views/_video.php
+++ src/widgets/views/_video.php
@@ -4,6 +4,7 @@
  * @var $banner \floor12\banner\models\AdsBanner
  * @var $place \floor12\banner\models\AdsPlace
  */
+$rand = rand(1000, 9999);
 ?>
 
 
@@ -11,12 +12,35 @@
 
     <?php if ($banner->href) { ?><a href="<?= $banner->href ?>"><?php } ?>
 
-        <video class="banner-desktop banner-video-desktop" autoplay muted playsinline loop data-id='<?= $banner->id ?>'
-               src="<?= $banner->file_desktop->getHref() ?>">
-        </video>
-        <video class="banner-mobile banner-video-mobile" autoplay muted playsinline loop data-id='<?= $banner->id ?>'
-               src="<?= $banner->file_mobile->getHref() ?>">
-        </video>
+        <?php if (!$banner->file_mobile): ?>
+            <video id="video<?= $rand ?>" autoplay muted playsinline loop data-id='<?= $banner->id ?>'
+                   src="<?= $banner->file_desktop->getHref() ?>">
+            </video>
+        <?php else: ?>
+            <video id="video<?= $rand ?>"  autoplay muted playsinline loop data-id='<?= $banner->id ?>'
+                   src="<?= $banner->file_mobile->getHref() ?>"
+                   data-desktop="<?= $banner->file_desktop->getHref() ?>">
+            </video>
+            <script>
+                // Получаем ссылку на элемент video
+                const videoElement = document.getElementById('video<?= $rand ?>');
+
+                // Получаем ширину окна браузера
+                const windowWidth = window.innerWidth;
+
+                // Заданный порог ширины окна браузера
+                const thresholdWidth = 800;
+
+                // Проверяем, является ли ширина окна больше порогового значения
+                if (windowWidth > thresholdWidth) {
+                    // Получаем значение атрибута data-desktop
+                    const desktopSrc = videoElement.getAttribute('data-desktop');
+
+                    // Устанавливаем значение атрибута src равным значению data-desktop
+                    videoElement.src = desktopSrc;
+                }
+            </script>
+        <?php endif; ?>
 
         <?php if ($banner->href){ ?></a><?php } ?>
 
