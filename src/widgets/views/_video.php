251811839de3081321f02e4c1e0f1df9386c6689
floor12 <floor12@floor12.net>
2023-06-07 01:00:33 +0200

    refactoring

--- src/widgets/views/_video.php
+++ src/widgets/views/_video.php
@@ -17,26 +17,14 @@ $rand = rand(1000, 9999);
                    src="<?= $banner->file_desktop->getHref() ?>">
             </video>
         <?php else: ?>
-            <video id="video<?= $rand ?>"  autoplay muted playsinline loop data-id='<?= $banner->id ?>'
+            <video id="video<?= $rand ?>" autoplay muted playsinline loop data-id='<?= $banner->id ?>'
                    src="<?= $banner->file_mobile->getHref() ?>"
                    data-desktop="<?= $banner->file_desktop->getHref() ?>">
             </video>
             <script>
-                // Получаем ссылку на элемент video
                 const videoElement = document.getElementById('video<?= $rand ?>');
-
-                // Получаем ширину окна браузера
-                const windowWidth = window.innerWidth;
-
-                // Заданный порог ширины окна браузера
-                const thresholdWidth = 800;
-
-                // Проверяем, является ли ширина окна больше порогового значения
-                if (windowWidth > thresholdWidth) {
-                    // Получаем значение атрибута data-desktop
+                if (window.innerWidth > 800) {
                     const desktopSrc = videoElement.getAttribute('data-desktop');
-
-                    // Устанавливаем значение атрибута src равным значению data-desktop
                     videoElement.src = desktopSrc;
                 }
             </script>
